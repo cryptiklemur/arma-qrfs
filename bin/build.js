ba6d6c1086ecd293a57440da877d2247b1150ab9
Aaron Scherer <aequasi@gmail.com>
2026-09-22 12:10:30 -0500

    build: delete the dead bin scripts
    hemtt release does all three jobs, and with no package.json nothing
    could call them anyway.

--- bin/build.js
+++
@@ -1,8 +0,0 @@
-const {resolve} = require('path');
-const {execSync} = require('child_process');
-
-const opts = {pwd: resolve(__dirname, '..'), stdio: 'inherit'};
-
-execSync('node .\\bin\\preBuild.js', opts);
-execSync('hemtt.exe build --release', opts);
-execSync('node .\\bin\\postBuild.js', opts);
